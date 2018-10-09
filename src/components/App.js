800a0718272ae6a93e6623ac631363a73d5682c9
CristiScarlat <78.cristi@gmail.com>
2018-10-09 17:11:34 +0300

    First commit React App and webpack starter

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return (
+        <div>
+            <h1>Simple React Test</h1>
+        </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import react from 'react';
+import reactDOM from 'react';
+import App from './components/App';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
